09b1cc8e5d487a8fa4e63a735f56181c2dd27eb2
Luiz Barboza <luiz.junior1@ibmec.edu.br>
2020-07-29 16:32:04 +0000

    ok

--- py1.py
+++ py1.py
@@ -1,7 +1,16 @@
 
 
 def nota_final(ap1, ap2, ap3):
+    return ap1*0.4 + ap2*0.4 + ap3*0.2
 
 def imc(peso,altura):
+    return peso/(altura*altura)
 
 def FtoC(f):
+  c = (5 * (f-32) / 9)
+  return c
+
+if __name__ == '__main__':
+  print(nota_final(8,8,8))
+  print(imc(100,100.0))
+  print(FtoC(122))
